dcf3042ca24b307b50f5cef65a3623a55dd7ce5b
Joy <baruelajoy@gmail.com>
2022-02-08 01:16:19 +0800

    stylized edit quiz page

--- resources/views/quizzes/create.blade.php
+++ resources/views/quizzes/create.blade.php
@@ -6,7 +6,7 @@
     </x-slot>
 
     {{-- BACK TO QUIZZES LIST --}}
-    <div class="py-12">
+    <div class="py-16">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <x-jet-button>
                 <a href="{{ route('quizzes.index') }}">
@@ -26,26 +26,23 @@
                             of Your Quiz, Sir?</label>
                         <input id="name" name="quiz[name]" type="text"
                             class="mb-4 mt-4 block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
-                            placeholder="Please provide a name of your quiz"></input>
+                            placeholder="Please provide a name for your quiz."></input>
                     </div>
                     <div>
                         <label for="description" class="text-xl font-medium text-gray-900">Provide a Description,
                             Sir.</label>
                         <textarea id="description" name="quiz[description]" rows="4"
                             class="mb-4 mt-4 block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
-                            placeholder="Please provide a description"></textarea>
+                            placeholder="Please provide a description for your quiz.">
+                        </textarea>
+                    </div>
+                    <div class="flex items-center mb-4">
+                        <input id="is_active" aria-describedby="quiz[is_active]" type="checkbox"
+                            class="w-4 h-4 bg-gray-50 rounded border border-gray-300 focus:ring-3 focus:ring-blue-300">
+                        <label for="is_active" class="ml-3 text-sm font-medium text-gray-900">Is your quiz active,
+                            Sir?</label>
+                    </div>
 
-                    </div>
-                    <div class="flex items-start mb-4">
-                        <div class="flex items-center">
-                            <input id="is_active" name="quiz[is_active]" type="checkbox"
-                                class="w-4 h-4 bg-gray-50 rounded border border-gray-300 focus:ring-3 focus:ring-blue-300">
-                        </div>
-                        <div class="ml-3 text-sm font-medium text-gray-900">
-                            <label for=" is_active" class="">Is your quiz active,
-                                Sir?</label>
-                        </div>
-                    </div>
                     <x-jet-button>
                         <input type="submit" class="font-semibold" value="DONE">
                     </x-jet-button>
--- resources/views/quizzes/edit.blade.php
+++ resources/views/quizzes/edit.blade.php
@@ -6,30 +6,46 @@
     </x-slot>
 
     {{-- BACK TO QUIZZES LIST --}}
-    <x-jet-button>
-        <a href="{{ route('quizzes.index') }}">
-            GO BACK
-        </a>
-    </x-jet-button>
-
-    {{-- FORM FOR QUIZ EDITION --}}
-    <div class="w-2/3  bg-orange-400 block">
-        <form method="POST" action="{{ route('quizzes.update', $quizItem->id) }}">
-            @csrf {{-- Little csrf guy protecting us from cross site attacks <3 --}}
-            @method('PUT') {{-- Put a little @method directive because we are in an Edit page. --}}
-            <label for="name">Edit the quiz name, Sir?</label> <br/>
-            <input id="name" name="quiz[name]" type="text" value="{{ $quizItem->name }}"> <br/>
-
-            <label for="description">Edit the Description, Sir?</label> <br/>
-            <input id="description" name="quiz[description]" type="text" value="{{ $quizItem->description }}"> <br/>
-
-            <label for="is_active">Is Your Quiz Still Active, Sir?</label> <br/>
-            <input id="is_active" name="quiz[is_active]" type="checkbox" @if ($quizItem->is_active === '1') checked="checked" @endif> <br/>
-
+    <div class="py-16">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <x-jet-button>
-                <input type="submit" value="Finish Editing">
+                <a href="{{ route('quizzes.index') }}">
+                    GO BACK
+                </a>
             </x-jet-button>
-        </form>
-    </div>
 
+            {{-- FORM FOR QUIZ EDITION --}}
+            <div class="block p-10 bg-white rounded-md shadow-sm">
+                <form method="POST" action="{{ route('quizzes.update', $quizItem->id) }}">
+                    @csrf {{-- Little csrf guy protecting us from cross site attacks <3 --}}
+                    @method('PUT') {{-- Put a little @method directive because we are in an Edit page. --}}
+                    <div>
+                        <label for="name"
+                            class="text-xl font-medium text-gray-900 after:content-['*'] after:ml-0.5 after:text-red-500">Edit
+                            the quiz name, Sir?</label>
+                        <input id="name" name="quiz[name]" type="text" value="{{ $quizItem->name }}"
+                            class="mb-4 mt-4 block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500">
+                    </div>
+                    <div>
+                        <label for="description" class=" mb-4 mt-4 text-xl font-medium text-gray-900">Edit the
+                            Description,
+                            Sir?</label>
+                        <input id="description" name="quiz[description]" value="{{ $quizItem->description }}"
+                            class="mb-4 mt-4 block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500">
+                        </input>
+                    </div>
+                    <div class="flex items-center mb-4">
+                        <input id="is_active" aria-describedby="quiz[is_active]" type="checkbox"
+                            class="w-4 h-4 bg-gray-50 rounded border border-gray-300 focus:ring-3 focus:ring-blue-300"
+                            @if ($quizItem->is_active === '1') checked="checked" @endif>
+                        <label for="is_active" class="ml-3 text-sm font-medium text-gray-900">Is Your Quiz
+                            Still Active, Sir?</label>
+                    </div>
+                    <x-jet-button>
+                        <input type="submit" class="font-semibold" value="FINISH EDITING">
+                    </x-jet-button>
+                </form>
+            </div>
+        </div>
+    </div>
 </x-app-layout>
