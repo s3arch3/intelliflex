bcae11fe7e1fa71426d8126d2527d0f457271bbd
Joy <baruelajoy@gmail.com>
2022-02-10 08:16:55 +0800

    stylized take quiz page

--- resources/views/quizzes/show.blade.php
+++ resources/views/quizzes/show.blade.php
@@ -4,45 +4,40 @@
             {{ __('Quiz Details') }}
         </h2>
     </x-slot>
-
-    {{-- BACK TO QUIZZES LIST --}}
-    <x-jet-button>
-        <a href="{{ route('quizzes.index') }}">
-            GO BACK
-        </a>
-    </x-jet-button>
-
-    {{-- CREATE QUESTION BUTTON --}}
-    <x-jet-button>
-        <a href="{{ route('questions.create', ['quizID' => $quizItem->id]) }}">
-            ADD QUESTION
-        </a>
-    </x-jet-button>
-
-    {{-- QUIZ DESCRIPTION --}}
-    <div class="w-2/3  bg-orange-400 block">
-        <form>
+    <div class="block px-24 py-4 bg-yellow-400 rounded-md shadow-sm mx-auto">
+        <form class="text-md leading-relaxed font-semibold text-gray-00">
             <label>QUIZ ID: {{ $quizItem->id }}</label> <br>
             <label>QUIZ NAME: {{ $quizItem->name }}</label> <br>
             <label>QUIZ DESC: {{ $quizItem->description }}</label> <br>
             <label>ACTIVE: @if ($quizItem->is_active === 1) YES @else NO @endif</label> <br>
         </form>
     </div>
-    <ul role="list" class="divide-y divide-gray-200 dark:divide-gray-700">
-        @foreach ($questions as $question)
-            {{-- {{ $question }} --}}
-            <x-question-list-item
-                :index="$loop->index+1"
-                :id="$question->id"
-                :question="$question->question"
-                :explanation="$question->explanation"
-                :isActive="$question->is_active"
-                :answers="$question->answers()->get()"/>
-            <br>
-        @endforeach
-    </ul>
+    {{-- BACK TO QUIZZES LIST --}}
+    <div class="py-4">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <x-jet-button>
+                <a href=" {{ route('quizzes.index') }}">
+                    GO BACK
+                </a>
+            </x-jet-button>
+            {{-- QUIZ DESCRIPTION --}}
 
-    {{-- QUESTIONS UNDER THIS QUIZ
+            <ul role="list" class="divide-y divide-gray-200 dark:divide-gray-700">
+                @foreach ($questions as $question)
+                    {{-- {{ $question }} --}}
+                    <x-question-list-item :index="$loop->index+1" :id="$question->id" :question="$question->question"
+                        :explanation="$question->explanation" :isActive="$question->is_active"
+                        :answers="$question->answers()->get()" />
+                    <br>
+                @endforeach
+            </ul>
+            {{-- CREATE QUESTION BUTTON --}}
+            <x-jet-button>
+                <a href="{{ route('questions.create', ['quizID' => $quizItem->id]) }}">
+                    ADD QUESTION
+                </a>
+            </x-jet-button>
+            {{-- QUESTIONS UNDER THIS QUIZ
     <ul role="list" class="divide-y divide-gray-200 dark:divide-gray-700">
         @foreach ($questions as $question)
             <x-question-list-item :questionNumber="$loop->index+1" :userID=null :quizID=null
@@ -53,5 +48,6 @@
                 :isCheckedA=null :isCheckedB=null :isCheckedC=null :isCheckedD=null />
         @endforeach
     </ul> --}}
-
+        </div>
+    </div>
 </x-app-layout>
