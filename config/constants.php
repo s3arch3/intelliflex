9e2de0e46fdac80ac0dc5ddba261b4adfcb9020a
Serge <21vibiesca@gmail.com>
2022-02-13 11:08:17 +0800

    migration for groups feature and added constants

--- config/constants.php
+++ config/constants.php
@@ -4,7 +4,11 @@ return [
     'quiz_description_max_length' => 255,
     'question_max_length' => 255,
     'answer_max_length' => 127,
-    'explanation_max_length' => 255
+    'explanation_max_length' => 255,
+
+    'group_code_length' => 7,
+    'group_name_max_length' => 32,
+    'group_description_max_length' => 255,
 ];
 // remember to run php artisan config:cache after adding a config file
 ?>
