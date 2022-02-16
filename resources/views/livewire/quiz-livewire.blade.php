7c51f72500c80441e1069cc568a287661d6bcbea
Joy <baruelajoy@gmail.com>
2022-02-16 20:00:50 +0800

    stylized take quiz continuation

--- resources/views/livewire/quiz-livewire.blade.php
+++ resources/views/livewire/quiz-livewire.blade.php
@@ -16,11 +16,11 @@
                 <br>
                 <div class="block p-10 bg-white rounded-md shadow-sm overflow-hidden">
                     <div class="flex flex-col items-center">
-                        <div class="flex items-center mb-4">
+                        <div class="flex items-center">
                             <div class="w-16 h-16 rounded-full p-4
                         shadow-md bg-gray-200">
                             </div>
-                            <p class="font-display ml-4 text-3xl font-bold text-blue-900">{{ $quizItem->name }}</p>
+                            <p class="font-display ml-4 text-3xl font-bold text-blue-600">{{ $quizItem->name }}</p>
                         </div>
                         <p class="mb-2">Quiz Livewire Start ID: {{ $quizID }}</p>
                         <p class="ml-4 text-sm font-regular text-gray-700 mb-2">{{ $quizItem->description }}
@@ -67,86 +67,89 @@
     {{-- QUIZ PROGRESS FRAME --}}
     {{-- QUIZ PROGRESS FRAME --}}
     @if ($quizInProgress)
-        <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="py-6">
+            <div class="max-w-4xl mx-auto sm:px-6 lg:px-8">
                 <div class="block p-10 bg-white rounded-md shadow-sm overflow-hidden">
-                    <div>
-                        <div class="mb-2">
-                            IN PROGRESS
+                    <div class="flex flex-col items-center">
+                        <div class="mb-2 font-display text-2xl text-blue-600">
+                            <b>Question # {{ $count }}</b>
                         </div>
-                        <div class="mb-2">
-                            <b>Quiz Progress</b>
+                        <div class="mb-6 font-display text-4xl text-gray-800">
+                            <b>{{ $currentQuestion->question }}</b>
                         </div>
-                        <div class="mb-2">
-                            <b>Question Number</b> {{ $count }}
+                        {{-- IMPORTANT --}}
+                        {{-- IMPORTANT --}}
+                        {{-- IMPORTANT --}}
+                        {{-- IMPORTANT --}}
+                        {{-- IMPORTANT --}}
+                        {{-- all radiobuttons must be enclosed in <div> wrapper because they should all have a root component --}}
+                        {{-- https://stackoverflow.com/questions/60395647/laravel-livewire-component-not-refreshing-reloading-automatically-after-refreshi --}}
+                        <div class="text-sm font-semibold mb-2 text-gray-800">
+                            {{-- list all the choices here --}}
+                            @foreach ($currentQuestion->answers as $answer)
+                                {{-- value property ex. 3,1 (answer_id, is_checked) 3 is the answer id and 1 is checked (0 is not) --}}
+                                <input
+                                    class="mb-2 w-4 h-4 bg-gray-50 rounded-full border border-gray-300 focus:ring-3 focus:ring-blue-300 "
+                                    type="radio" id="answer" name="selectedAnswer" wire:model="userAnswered"
+                                    value="{{ $answer->id . ',' . $answer->is_checked }}">
+                                {{ $answer->answer }}
+                                <br>
+                                <br>
+                            @endforeach
                         </div>
-                        <div class="mb-2">
-                            <b>Question</b> {{ $currentQuestion->question }}
-                        </div>
+                        {{-- "Next Question" or "Show Results" --}}
+                        <form wire:submit.prevent="nextQuestion">
+                            @csrf
+                            @if ($count < $questionsCount)
+                                <x-jet-button class="font-bold">
+                                    <input type="submit" value="NEXT QUESTION">
+                                </x-jet-button>
+                            @else
+                                <x-jet-button class="font-bold">
+                                    <input type="submit" value="SHOW RESULTS">
+                                </x-jet-button>
+                            @endif
+                        </form>
+
                     </div>
-
-                    {{-- IMPORTANT --}}
-                    {{-- IMPORTANT --}}
-                    {{-- IMPORTANT --}}
-                    {{-- IMPORTANT --}}
-                    {{-- IMPORTANT --}}
-                    {{-- all radiobuttons must be enclosed in <div> wrapper because they should all have a root component --}}
-                    {{-- https://stackoverflow.com/questions/60395647/laravel-livewire-component-not-refreshing-reloading-automatically-after-refreshi --}}
-                    <div>
-                        {{-- list all the choices here --}}
-                        @foreach ($currentQuestion->answers as $answer)
-                            {{-- value property ex. 3,1 (answer_id, is_checked) 3 is the answer id and 1 is checked (0 is not) --}}
-                            <input type="radio" id="answer" name="selectedAnswer" wire:model="userAnswered"
-                                value="{{ $answer->id . ',' . $answer->is_checked }}">
-                            {{ $answer->answer }}
-                            <br>
-                        @endforeach
-                    </div>
-
-
-                    {{-- "Next Question" or "Show Results" --}}
-                    <form wire:submit.prevent="nextQuestion">
-                        @csrf
-                        @if ($count < $questionsCount)
-                            <x-jet-button class="font-bold">
-                                <input type="submit" value="NEXT QUESTION">
-                            </x-jet-button>
-                        @else
-                            <x-jet-button class="font-bold">
-                                <input type="submit" value="SHOW RESULTS">
-                            </x-jet-button>
-                        @endif
-                    </form>
-
                 </div>
             </div>
-        </div>
     @endif
     {{-- QUIZ END FRAME --}}
     {{-- QUIZ END FRAME --}}
     {{-- QUIZ END FRAME --}}
     @if ($quizInEnd)
         {{-- wire:poll.500ms --}}
-        <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="py-6">
+            <div class="max-w-4xl mx-auto sm:px-6 lg:px-8">
                 <div class="block p-10 bg-white rounded-md shadow-sm overflow-hidden">
-                    <div class="grid place-items-center font-bold text-lg">
-                        IN END
-                    </div>
-                    {{-- IMPORTANT --}}
-                    {{-- IMPORTANT --}}
-                    {{-- IMPORTANT --}}
-                    {{-- enclose livewire stuff in a div --}}
-                    <div>
-                        <div class="mb-2">
-                            <b>You Completed the Quiz!</b>
-                            ID: {{ $quizID }}
+                    <div class="flex flex-col items-center text-center ">
+                        {{-- IMPORTANT --}}
+                        {{-- IMPORTANT --}}
+                        {{-- IMPORTANT --}}
+                        {{-- enclose livewire stuff in a div --}}
+                        <img src="{{ asset('assets/illustrations/confetti.png') }}" class="w-10 h-10 mb-4" alt="">
+                        <p class="font-display font-semibold text-4xl text-gray-800">Congratulations, you
+                            completed the</p>
+                        {{-- ID: {{ $quizID }} --}}
+                        <div class="mb-6 font-display text-4xl inline-flex">
+                            <p class="font-bold text-blue-600">{{ $quizItem->name }} </p>
+                            <p class="ml-2 font-semibold text-gray-800">quiz!
                         </div>
-                        <div class="mb-2">
-                            <b>Quiz Title</b> {{ $quizItem->name }}
-                        </div>
-                        <div class="mb-2">
+                        {{-- <div class="mb-2">
                             <b>Description</b> {{ $quizItem->description }}
+                        </div> --}}
+                        <div class="mb-2 text-gray-800">
+                            <p class="mb-2 text-lg font-semibold">Your score is</p>
+                            <p class="
+                               mb-4 text-4xl font-bold">
+                                {{ $correctAnswers }} out of
+                                {{ $questionsCount }}
+
+                        </div>
+                        <div class="text-lg inline-flex">
+                            <p class="font-semibold text-gray-800">You completed this quiz
+                            <p class="ml-2 font-bold text-blue-600">{{ $quizItem->times_completed }} times
                         </div>
                         <div class="mb-2">
                             <b>Times Completed</b> {{ $quizItem->times_completed }}
@@ -154,36 +157,34 @@
                         <div class="mb-2">
                             <b>Questions</b> {{ $questionsCount }}
                         </div>
-                        <div class="mb-2">
-                            <b>Score</b> {{ $correctAnswers }}
-                        </div>
+
                         <div class="mb-2">
                             <b>Percentage</b> {{ $quizPercentage }}
                         </div>
                     </div>
-
-                    {{-- no submit.prevent to produce default action which is refreshing and going to details page --}}
-                    <x-jet-button>
-                        <a href="{{ route('log', $quizLog->id) }}">
-                            Full Details
-                        </a>
-                    </x-jet-button>
-
-                    {{-- BACK TO HOME --}}
-                    <x-jet-button class="ml-2">
-                        <a href="{{ route('home') }}">
-                            Return to Home
-                        </a>
-                    </x-jet-button>
-
-                    <form wire:submit.prevent="startQuiz">
-                        @csrf
-                        <x-jet-button class="font-semibold">
-                            <input type="submit" value="TAKE QUIZ AGAIN">
-                        </x-jet-button>
-                    </form>
                 </div>
+                {{-- no submit.prevent to produce default action which is refreshing and going to details page --}}
+                <x-jet-button>
+                    <a href="{{ route('log', $quizLog->id) }}">
+                        Full Details
+                    </a>
+                </x-jet-button>
+
+                {{-- BACK TO HOME --}}
+                <x-jet-button class="ml-2">
+                    <a href="{{ route('home') }}">
+                        Return to Home
+                    </a>
+                </x-jet-button>
+
+                <form wire:submit.prevent="startQuiz">
+                    @csrf
+                    <x-jet-button class="font-semibold">
+                        <input type="submit" value="TAKE QUIZ AGAIN">
+                    </x-jet-button>
+                </form>
             </div>
         </div>
-    @endif
+</div>
+@endif
 </div>
