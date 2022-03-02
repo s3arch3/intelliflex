44f99b80ab839104ff014e667cbb5e24884f3061
Joy <baruelajoy@gmail.com>
2022-03-02 19:22:15 +0800

    stylized user statistics cards

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,33 +4,39 @@
             {{ __('Home') }}
         </h2>
     </x-slot>
-    <div class="py-12">
+    <div class="py-6">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class=" block p-10 bg-white rounded-md shadow-sm overflow-hidden">
                 <div class="font-bold text-xl"> User Statistics
                 </div>
                 <br>
-                <div class="flex text-center">
-                    <div class="mb-2 rounded-md border border-gray-200 p-6">
-                        <img src="{{ asset('assets/illustrations/home') }}" class="mx-auto w-10" alt="">
-                        <p class="text-blue-800 text-md">Quiz Count</p>
-                        <p class="text-4xl font-bold text-blue-800">{{ $quizCount }}</p>
+                <div class="flex flex-wrap justify-center text-center">
+                    <div class="mb-2 rounded-md border border-gray-200 p-6 shadow-md m-2">
+                        <img src="{{ asset('assets/illustrations/home/quiz-count.png') }}" class="mb-2 mx-auto w-10"
+                            alt="">
+                        <b class="text-gray-800 text-sm mb-2">Quiz Count</b>
+                        <p class="text-4xl font-bold text-blue-600">{{ $quizCount }}</p>
                     </div>
-                    <div class="mb-2">
-                        <b>Overall Accuracy:</b>
-                        {{ $overallAccuracy }} <br>
+                    <div class="mb-2 rounded-md border border-gray-200 p-6 shadow-md m-2">
+                        <img src="{{ asset('assets/illustrations/home/accuracy.png') }}" class="mb-2 mx-auto w-10">
+                        <b class="text-gray-800 text-sm mb-2">Overall Accuracy:</b>
+                        <p class="text-4xl font-bold text-blue-600">{{ $overallAccuracy }}</p>
                     </div>
-                    <div class="mb-2">
-                        <b>Total Questions Answered:</b>
-                        {{ $totalQuestionsAnswered }} <br>
+                    <div class="mb-2 rounded-md border border-gray-200 p-6 shadow-md m-2">
+                        <img src="{{ asset('assets/illustrations/home/questions.png') }}" class="mb-2 mx-auto w-10">
+                        <b class="text-gray-800 text-sm mb-2">Total Questions Answered:</b>
+                        <p class="text-4xl font-bold text-blue-600">
+                            {{ $totalQuestionsAnswered }}</p>
                     </div>
-                    <div class="mb-2">
-                        <b class="mb-2">Total Points:</b>
-                        {{ $totalPoints }} <br>
+                    <div class="mb-2 rounded-md border border-gray-200 p-6 shadow-md m-2">
+                        <img src="{{ asset('assets/illustrations/home/points.png') }}" class="mb-2 mx-auto w-10">
+                        <b class="text-gray-800 text-sm mb-2">Total Points:</b>
+                        <p class="text-4xl font-bold text-blue-600"> {{ $totalPoints }}</p>
                     </div>
-                    <div class="mb-2">
-                        <b>Login Streak:</b>
-                        {{ 'N/A' }} <br>
+                    <div class="mb-2 rounded-md border border-gray-200 p-6 shadow-md m-2">
+                        <img src="{{ asset('assets/illustrations/home/login.png') }}" class="mb-2 mx-auto w-10">
+                        <b class="text-gray-800 text-sm mb-2">Login Streak:</b>
+                        <p class="text-3xl font-bold text-blue-600">{{ 'N/A' }}</p>
                     </div>
                 </div>
                 <br>
