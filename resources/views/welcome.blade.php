d83e8ef182c4994ef7647c7b056b6eee158d029a
Joy <baruelajoy@gmail.com>
2022-03-03 20:15:16 +0800

    uploaded illustrations for home page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,7 +14,7 @@
             </a>
             <div class="flex">
                 <button type="button"
-                    class="text-blue-600 bg-slate-100 hover:bg-blue-200 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-slate-100 dark:hover:bg-blue-200 dark:focus:ring-slate-300">LOGIN
+                    class="text-blue-600 bg-slate-100 hover:bg-blue-100 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-slate-100 dark:hover:bg-blue-100 dark:focus:ring-slate-300">LOGIN
                 </button>
                 <button type="button"
                     class="text-white bg-blue-600 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-200 ml-4 shadow-lg">REGISTER
@@ -24,7 +24,13 @@
 </head>
 
 <body>
-
+    <div class="container mx-auto flex flex-wrap items-center">
+        <img src="{{ asset('/assets/illustrations/landing-page/hero-image.png') }}" alt="hero-image"
+            class="mt-10">
+        <div class="flex flex-col items-center justify-between p-4">
+            <h1 class="text-4xl font-bold font-display tracking-tight">Remaster your knowledge.</h1>
+        </div>
+    </div>
 </body>
 
 </html>
