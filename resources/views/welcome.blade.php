6ddacfd038d0bea13000b043e5882eaff1b6eb70
Joy <baruelajoy@gmail.com>
2022-03-03 23:23:23 +0800

    initial home banner

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -24,11 +24,24 @@
 </head>
 
 <body>
-    <div class="container mx-auto flex flex-wrap items-center">
+    <div class="container mx-auto flex flex-initial items-center">
         <img src="{{ asset('/assets/illustrations/landing-page/hero-image.png') }}" alt="hero-image"
-            class="mt-10">
-        <div class="flex flex-col items-center justify-between p-4">
-            <h1 class="text-4xl font-bold font-display tracking-tight">Remaster your knowledge.</h1>
+            class="mt-10 sm:w-32 md:w-72 lg:w-96 xl:w-auto">
+        <div class="py-8 px-10 text-gray-800">
+            <h1 class="text-5xl font-bold font-display tracking-tight my-4">Remaster your knowledge.</h1>
+            <p class="mt-2 font-normal pr-4">Repeat, create, and share quizzes with your peers in the name of
+                knowledge and
+                learning on any device.</p>
+            <button type="button"
+                class="text-white bg-blue-600 hover:bg-blue-900 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 mt-6 w-auto">GET
+                STARTED
+            </button>
+        </div>
+    </div>
+    <div class="container mx-auto text-center">
+        <div class="font-bold font-display text-3xl my-8 text-gray-800">Features</div>
+        <div class="flex flex-col">
+            <div class="bg-slate-100 w-16"></div>
         </div>
     </div>
 </body>
