ffbd3e263d018fe30f26e5d8992de73b86f79517
Serge <21vibiesca@gmail.com>
2022-03-08 01:47:48 +0800

    Updated Groups header

--- resources/views/groups/index.blade.php
+++ resources/views/groups/index.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('All Groups') }}
+        <h2 class="font-display font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Groups') }}
         </h2>
     </x-slot>
     @if ($userType == 'student')
