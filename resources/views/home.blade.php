be41b7d3cede1af5dc411f1b484fe1aaf73802a3
Joy <baruelajoy@gmail.com>
2022-03-29 02:20:03 +0800

    modified home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,53 +4,59 @@
             {{ __('Home') }}
         </h2>
     </x-slot>
-    <div class="py-12">
+    <div class="py-12 px-4">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="mb-6">
-                <p class=" font-display font-bold text-3xl mb-2"> Welcome back,
-                    <span class="text-blue-600"> {{ $userName }}</span>!
+                <p class=" font-display font-medium text-3xl mb-2 "> Welcome back,
+                    <span class="font-bold"> {{ $userName }}!</span>
                 </p>
-                <p>Here are your total performance metrics on this application.</p>
+                <p class="text-sm">Here are your total performance metrics on this application.</p>
             </div>
-            <div class=" block p-10 pt-4 bg-white rounded-md shadow-sm overflow-hidden">
+            <div class="block p-4 bg-white rounded-md shadow-sm overflow-hidden border-gray-100">
                 <br>
-                <p class="font-bold text-xl mb-2">Performance Overview </p>
-                <div class="flex flex-wrap justify-center text-center">
-                    <div class="mb-2 rounded-md bg-slate-100 p-6 m-2">
+                <p class="font-semibold text-xl mb-4 sm:text-left lg:text-left md:text-left text-center">
+                    Performance
+                    Overview
+                </p>
+                <div class="flex flex-wrap justify-center lg:justify-start text-center">
+                    <div class="w-36 md:w-48 rounded-md bg-blue-50 p-6 text-cyan-600 m-2">
                         <img src="{{ asset('assets/illustrations/home/quiz-count.png') }}" class="mb-2 mx-auto w-10"
                             alt="">
-                        <b class="text-gray-800 text-sm mb-2">Quiz Count:</b>
-                        <p class="text-4xl font-bold text-blue-600">{{ $quizCount }}</p>
+                        <b class="text-sm mb-2">Quiz Count:</b>
+                        <p class="text-2xl font-bold md:text-4xl">{{ $quizCount }}</p>
                     </div>
-                    <div class="mb-2 rounded-md bg-slate-100 p-6 m-2">
+                    <div class="w-36 md:w-48 rounded-md bg-pink-50 p-6 text-pink-500 m-2">
                         <img src="{{ asset('assets/illustrations/home/accuracy.png') }}" class="mb-2 mx-auto w-10">
-                        <b class="text-gray-800 text-sm mb-2">Overall Accuracy:</b>
-                        <p class="text-4xl font-bold text-blue-600">{{ $overallAccuracy }}</p>
+                        <b class="text-sm mb-2">Overall Accuracy:</b>
+                        <p class="text-2xl font-bold md:text-4xl">{{ $overallAccuracy }}</p>
                     </div>
-                    <div class="mb-2 rounded-md bg-slate-100 p-6 m-2">
+                    <div class="
+                        w-36 md:w-48 rounded-md bg-indigo-50 p-6 text-indigo-600 m-2">
                         <img src="{{ asset('assets/illustrations/home/questions.png') }}" class="mb-2 mx-auto w-10">
-                        <b class="text-gray-800 text-sm mb-2">Total Questions Answered:</b>
-                        <p class="text-4xl font-bold text-blue-600">
+                        <b class="text-sm mb-2">Total Questions Answered:</b>
+                        <p class="text-2xl font-bold md:text-4xl">
                             {{ $totalQuestionsAnswered }}</p>
                     </div>
-                    <div class="mb-2 rounded-md bg-slate-100 p-6 m-2">
+                    <div class="w-36 md:w-48 rounded-md bg-amber-50 p-6 text-amber-600 m-2">
                         <img src="{{ asset('assets/illustrations/home/points.png') }}" class="mb-2 mx-auto w-10">
-                        <b class="text-gray-800 text-sm mb-2">Total Points:</b>
-                        <p class="text-4xl font-bold text-blue-600"> {{ $totalPoints }}</p>
+                        <b class="text-sm mb-2">Total Points:</b>
+                        <p class="text-2xl font-bold md:text-4xl"> {{ $totalPoints }}</p>
                     </div>
-                    <div class="mb-2 rounded-md bg-slate-100 p-6 m-2">
+                    <div class="w-36 md:w-48 rounded-md bg-emerald-50 text-emerald-500 p-6 m-2">
                         <img src="{{ asset('assets/illustrations/home/login.png') }}" class="mb-2 mx-auto w-10">
-                        <b class="text-gray-800 text-sm mb-2">Login Streak:</b>
-                        <p class="text-3xl font-bold text-blue-600">{{ 'N/A' }}</p>
+                        <b class="text-sm mb-2">Login Streak:</b>
+                        <p class="text-2xl font-bold md:text-4xl">{{ 'N/A' }}</p>
                     </div>
                 </div>
-                <br>
+            </div>
+            <div class="block p-8 pt-4 mt-4 bg-white border border-gray-100 rounded-md shadow-sm overflow-hidden">
                 {{-- <div class="font-bold text-lg mb-10">
                     <p>Chart about points earned in this week (Mon-Sun)</p>
                     <br>
                     <i>Chart Here</i>
                 </div> --}}
-                <div class="font-semibold text-lg mb-4"> Quiz History</div>
+                <div class="font-semibold text-xl mb-4 sm:text-left lg:text-left md:text-left text-center"> Quiz History
+                </div>
                 {{-- code for quiz logs list --}}
                 @foreach ($quizLogs as $quizLog)
                     <x-quiz-log-item :id="
@@ -60,6 +66,4 @@
                 @endforeach
             </div>
         </div>
-    </div>
-    </div>
 </x-app-layout>
