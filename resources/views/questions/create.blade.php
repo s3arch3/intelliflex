caeb2e0f7ad0587fdaec500e0daeeab901028d48
Joy <baruelajoy@gmail.com>
2022-04-08 22:59:07 +0800

    stylized create question page

--- resources/views/questions/create.blade.php
+++ resources/views/questions/create.blade.php
@@ -4,59 +4,90 @@
             {{ __('Create Question') }}
         </h2>
     </x-slot>
-
-    {{-- BACK TO THAT QUIZ ITEM DETAILS --}}
-    <x-jet-button>
-        <a href="{{ route('quizzes.index') }}">
-            GO BACK
-        </a>
-    </x-jet-button>
-
-    {{-- CREATE QUESTION FORM --}}
-    <div class="w-2/3  bg-orange-400 block">
-        <form method="POST" action="{{ route('questions.store', ['quizID' => $quizID]) }}">
-            @csrf {{-- Little csrf guy protecting us from cross site attacks <3 --}}
-            <label for="quizID">Creating question for Quiz ID: {{ $quizID }}</label> <br>
-
-
-            <label for="question">Question Name, Sir?</label> <br>
-            <input id="question" name="question[question]" type="text"> <br>
-
-            <label for="explanation">Explanation of the Question, Sir. Will be used as a secret tool (hint) later.
-                ;)</label> <br>
-            <input id="explanation" name="question[explanation]" type="text"> <br>
-
-            <br>
-            <input id="is_active" name="question[is_active]" type="checkbox" checked="checked">
-            <label for="is_active">Is Your Question Active, Sir?</label> <br>
-
-            <br>
-            {{-- CHOCIES --}}
-            A
-            <input type="radio" id="answerA" name="question[is_selected]" value="A">
-            <input id="answerA" name="answers[0][answer]" type="text" placeholder="answer for letter A">
-            <input id="explanationA" name="answers[0][explanation]" type="text" placeholder="explanation for letter A"> <br/>
-
-            B
-            <input type="radio" id="answerB" name="question[is_selected]" value="B">
-            <input id="answerB" name="answers[1][answer]" type="text" placeholder="answer for letter B">
-            <input id="explanationB" name="answers[1][explanation]" type="text" placeholder="explanation for letter B"> <br/>
-
-            C
-            <input type="radio" id="answerC" name="question[is_selected]" value="C">
-            <input id="answerC" name="answers[2][answer]" type="text" placeholder="answer for letter C">
-            <input id="explanationC" name="answers[2][explanation]" type="text" placeholder="explanation for letter C"> <br/>
-
-            D
-            <input type="radio" id="answerD" name="question[is_selected]" value="D">
-            <input id="answerD" name="answers[3][answer]" type="text" placeholder="answer for letter D">
-            <input id="explanationD" name="answers[3][explanation]" type="text" placeholder="explanation for letter D"> <br/>
-
-            <br/><br/>
+    <div class="py-4 max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="py-4 px-4 lg:px-0">
+            {{-- BACK TO THAT QUIZ ITEM DETAILS --}}
             <x-jet-button>
-                <input type="submit" value="Question Making I'm Done Sire.">
+                <a href="{{ route('quizzes.index') }}">
+                    GO BACK
+                </a>
             </x-jet-button>
-        </form>
-    </div>
+            {{-- CREATE QUESTION FORM --}}
+            <div class="block p-10 bg-white rounded-md shadow-sm">
+                <form method="POST" action="{{ route('questions.store', ['quizID' => $quizID]) }}">
+                    @csrf {{-- Little csrf guy protecting us from cross site attacks <3 --}}
+                    <div class="mb-4">
+                        <label for="quizID" class="text-sm font-normal">Creating question for Quiz ID
+                            #{{ $quizID }}</label>
+                    </div>
+                    <div>
+                        <label for="question"
+                            class="font-display text-md font-bold after:content-['*'] after:ml-0.5 after:text-red-500 text-blue-800">Question
+                            name</label>
+                        <input id="question" name="question[question]" type="text"
+                            class="mb-4 mt-2 block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500">
+                    </div>
+                    <div>
+                        <label for="explanation"
+                            class="font-display text-md font-bold after:content-['*'] after:ml-0.5 after:text-red-500 text-blue-800">Explanation
+                            <span class="text-sm font-medium">(Will be used as a secret tool or hint
+                                later.)</span></label>
+                        <input id="explanation" name="question[explanation]" type="text"
+                            class="mb-4 mt-2 block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500">
+                    </div>
+                    <input id="is_active" name="question[is_active]" type="checkbox" checked="checked"
+                        class="w-4 h-4 bg-gray-50 rounded border border-gray-300 focus:ring-3 focus:ring-blue-300">
+                    <label for="is_active" class=" ml-2 text-sm font-medium text-gray-900">Set Active
+                        Status</label>
+            </div>
+            <div class="py-8">
+                <p class="font-display font-bold text-2xl mb-2">Choices
+                </p>
+                {{-- CHOICES --}}
+                <div class="block p-10 bg-white rounded-md shadow-sm">
+                    <div class="mb-4">
+                        <input type="radio" id="answerA" name="question[is_selected]" value="A">
+                        <label for="" class="font-display text-md font-bold text-blue-800">A
+                        </label>
+                        <input id="answerA" name="answers[0][answer]" type="text" placeholder="answer for letter A"
+                            class="mb-2 mt-2 p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500">
+                        <input id="explanationA" name="answers[0][explanation]" type="text"
+                            placeholder="explanation for letter A"
+                            class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500">
+                    </div>
+                    <div class="mb-4">
+                        <input type="radio" id="answerB" name="question[is_selected]" value="B">
+                        <label for="" class="font-display text-md font-bold text-blue-800">B
+                        </label>
+                        <input id="answerB" name="answers[1][answer]" type="text" placeholder="answer for letter B"
+                            class="mb-2 mt-2 p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500">
+                        <input id="explanationB" name="answers[1][explanation]" type="text"
+                            placeholder="explanation for letter B"
+                            class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500">
+                    </div>
+                    <div class="mb-4">
+                        <input type="radio" id="answerC" name="question[is_selected]" value="C">
+                        <label for="" class="font-display text-md font-bold text-blue-800">C
+                        </label>
+                        <input id="answerC" name="answers[2][answer]" type="text" placeholder="answer for letter C"
+                            class="mb-2 mt-2 p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500">
+                        <input id="explanationC" name="answers[2][explanation]" type="text"
+                            placeholder="explanation for letter C"
+                            class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500">
+                    </div>
+                    <div class="mb-4">
+                        <input type="radio" id="answerD" name="question[is_selected]" value="D">
+                        <label for="" class="font-display text-md font-bold text-blue-800">D
+                        </label>
+                        <input id="answerD" name="answers[3][answer]" type="text" placeholder="answer for letter D"
+                            class="mb-2 mt-2 p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500">
+                        <input id="explanationD" name="answers[3][explanation]" type="text"
+                            placeholder="explanation for letter D"
+                            class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500">
+                    </div>
+                    <x-jet-button>
+                        <input type="submit" value="SAVE" class="text-md font-medium">
+                    </x-jet-button>
+                </div>
 
 </x-app-layout>
