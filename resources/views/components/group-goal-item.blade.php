eeef4bf3e110b865caf591236e96baf77f428bcb
Joy <baruelajoy@gmail.com>
2022-04-10 01:58:47 +0800

    modified collective achievements

--- resources/views/components/group-goal-item.blade.php
+++ resources/views/components/group-goal-item.blade.php
@@ -1,28 +1,28 @@
 {{-- THIS COMPONENT IS FOR A GROUP GOAL ITEM --}}
 <div class="w-80 text-center bg-white rounded-md p-4 border border-bg-gray-300">
     {{-- GROUP GOAL LOGO --}}
-    <img src="{{ asset('assets/illustrations/goals-icon/1-deja-vu.png') }}" class="mx-auto w-16 h-16 mb-4" alt="">
+    <img src="{{ asset('assets/illustrations/goals-icon/1-deja-vu.png') }}" class="mx-auto w-16 h-16 mb-2" alt="">
 
     {{-- ACHIEVEMENT STATUS CHIPS --}}
     @if ($achieved == true)
         <div
-            class="mx-auto bg-emerald-50 border border-emerald-300 rounded-full text-emerald-500 font-semibold text-sm w-28">
+            class="mx-auto bg-emerald-50 border border-emerald-300 rounded-full text-emerald-500 font-semibold text-sm w-28 my-2">
             Acquired
         </div>
     @else
         <div
-            class="mx-auto bg-amber-50 border border-amber-300 rounded-full text-amber-500 font-semibold text-sm w-28">
+            class="mx-auto bg-amber-50 border border-amber-300 rounded-full text-amber-500 font-semibold text-sm w-28 my-2">
             {{ $totalGroupPoints . '/' . $goal }}
         </div>
     @endif
 
     {{-- GROUP GOAL NAME --}}
-    <b class="m-2 text-blue-600">
+    <b class="text-blue-600">
         {{ $goalName }}
     </b>
 
     {{-- GROUP GOAL DESCRIPTION --}}
-    <p class="m-2 text-sm text-gray-600">
+    <p class="text-sm text-gray-600">
         {{ $goalDescription }}
     </p>
 </div>
